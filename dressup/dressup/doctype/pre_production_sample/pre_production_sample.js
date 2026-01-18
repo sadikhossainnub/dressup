2e5053369d6ef8d70e7dee537ea687d4ebe78090
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-01-19 02:59:23 +0600

    feat: Update print formats to use "Style No" instead of "Design No" and enhance Cost Estimation doctype with new fields, logic, and UI adjustments.

--- dressup/dressup/doctype/pre_production_sample/pre_production_sample.js
+++ dressup/dressup/doctype/pre_production_sample/pre_production_sample.js
@@ -4,13 +4,13 @@
 frappe.ui.form.on("Pre Production Sample", {
 	refresh(frm) {
 		if (!frm.doc.__islocal && frm.doc.docstatus === 0) {
-			frm.add_custom_button(__('Fetch Tech Pack Data'), function() {
+			frm.add_custom_button(__('Fetch Tech Pack Data'), function () {
 				frm.trigger('fetch_tech_pack_data');
 			});
 		}
-		
+
 		if (frm.doc.docstatus === 1) {
-			frm.add_custom_button(__('Quality Inspection'), function() {
+			frm.add_custom_button(__('Quality Inspection'), function () {
 				frappe.model.open_mapped_doc({
 					method: 'dressup.dressup.doctype.pre_production_sample.pre_production_sample.make_quality_inspection',
 					frm: frm
@@ -18,13 +18,13 @@ frappe.ui.form.on("Pre Production Sample", {
 			}, __('Create'));
 		}
 	},
-	
+
 	tech_pack_no(frm) {
 		if (frm.doc.tech_pack_no) {
 			frm.trigger('fetch_tech_pack_data');
 		}
 	},
-	
+
 	onload(frm) {
 		if (frm.is_new()) {
 			setTimeout(() => {
@@ -34,7 +34,7 @@ frappe.ui.form.on("Pre Production Sample", {
 			}, 500);
 		}
 	},
-	
+
 	add_default_size_chart(frm) {
 		const sizes = ['36', '38', '40', '42'];
 		sizes.forEach(size => {
@@ -44,10 +44,10 @@ frappe.ui.form.on("Pre Production Sample", {
 		});
 		frm.refresh_field('size_chart_in_inch');
 	},
-	
+
 	fetch_tech_pack_data(frm) {
 		if (!frm.doc.tech_pack_no) return;
-		
+
 		frappe.call({
 			method: 'frappe.client.get',
 			args: {
@@ -57,29 +57,53 @@ frappe.ui.form.on("Pre Production Sample", {
 			callback(r) {
 				if (r.message) {
 					let tech_pack = r.message;
-					
+
 					// Auto-populate Quality Inspection Template
 					if (!frm.doc.link_nsnp) {
-						frappe.db.get_list('Quality Inspection Template', {limit: 1}).then(templates => {
+						frappe.db.get_list('Quality Inspection Template', { limit: 1 }).then(templates => {
 							if (templates.length > 0) {
 								frm.set_value('link_nsnp', templates[0].name);
 							}
 						});
 					}
-					
+
 					// Auto-populate disclaimer
 					if (!frm.doc.read_confirm_carefully) {
 						frm.set_value('read_confirm_carefully', 'I confirm that all measurements, materials, and specifications have been reviewed and are accurate.');
 					}
-					
+
 					// Set start time
 					if (!frm.doc.start_time_date) {
 						frm.set_value('start_time_date', frappe.datetime.now_datetime());
 					}
-					
-					frappe.show_alert({message: __('Tech Pack data fetched successfully'), indicator: 'green'});
+
+					frappe.show_alert({ message: __('Tech Pack data fetched successfully'), indicator: 'green' });
 				}
 			}
 		});
 	}
 });
+
+frappe.ui.form.on("PPS Fabric Item", {
+	actual_quantity: function (frm, cdt, cdn) {
+		calculate_child_amount(frm, cdt, cdn);
+	},
+	rate: function (frm, cdt, cdn) {
+		calculate_child_amount(frm, cdt, cdn);
+	}
+});
+
+frappe.ui.form.on("PPS Trim Accessories Item", {
+	actual_quantity: function (frm, cdt, cdn) {
+		calculate_child_amount(frm, cdt, cdn);
+	},
+	rate: function (frm, cdt, cdn) {
+		calculate_child_amount(frm, cdt, cdn);
+	}
+});
+
+function calculate_child_amount(frm, cdt, cdn) {
+	let row = locals[cdt][cdn];
+	let amount = (flt(row.actual_quantity) || 0) * (flt(row.rate) || 0);
+	frappe.model.set_value(cdt, cdn, "amount", amount);
+}
