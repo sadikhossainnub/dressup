ddb2aff67a775380948ae63f442567d3ad3d2d20
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-02-04 19:06:59 +0600

    feat: Add DocTypes for defining barcode label templates and generating printable labels.

--- dressup/barcode_label_print/doctype/barcode_label_print/barcode_label_print.py
+++ dressup/barcode_label_print/doctype/barcode_label_print/barcode_label_print.py
@@ -57,14 +57,8 @@ class BarcodeLabelPrint(Document):
 		">
 		"""
 		
-		# Check for Visual Design Data
-		import json
+		# Use simple layout (visual designer removed)
 		design_elements = []
-		if template.design_data:
-			try:
-				design_elements = json.loads(template.design_data)
-			except:
-				pass
 
 		for data in items_data:
 			# If we have a visual design, use absolute positioning
@@ -121,63 +115,119 @@ class BarcodeLabelPrint(Document):
 				label_html += "</div>"
 				
 			else:
-				# Fallback to Old Flex Layout
+				# Fallback to Simple Layout with all checkbox options
 				label_html = f"""
 				<div style="
 					width: {template.label_width}mm;
 					height: {template.label_height}mm;
 					box-sizing: border-box;
-					padding: 0;
-					font-family: sans-serif;
+					padding: 2mm;
+					font-family: Arial, sans-serif;
 					font-size: {template.font_size}px;
 					overflow: hidden;
 					background: white;
 					page-break-inside: avoid;
-					position: relative;
+					border: 1px solid #000;
+					display: flex;
+					flex-direction: column;
+					justify-content: space-between;
 				">
-					<div style="
-						border: 1.2pt solid #000000;
-						width: 90%;
-						height: 80%;
-						margin-top: 10%;
-						margin-left: 2pt;
-						display: flex;
-						flex-direction: column;
-						box-sizing: border-box;
-					">
 				"""
 				
-				if template.get("include_price") and data.get("price"):
+				# Company Name (Top)
+				if template.include_company_name and data.get("company"):
+					label_html += f"""
+						<div style="
+							text-align: center;
+							font-weight: bold;
+							font-size: {template.font_size * 1.1}px;
+							border-bottom: 1px solid #ccc;
+							padding-bottom: 1mm;
+							margin-bottom: 1mm;
+						">{data['company']}</div>
+					"""
+				
+				# Item Name
+				if template.include_item_name and data.get("item_name"):
+					label_html += f"""
+						<div style="
+							text-align: center;
+							font-size: {template.font_size}px;
+							margin-bottom: 1mm;
+							overflow: hidden;
+							text-overflow: ellipsis;
+							white-space: nowrap;
+						">{data['item_name']}</div>
+					"""
+				
+				# Item Code
+				if template.include_item_code and data.get("item_code"):
+					label_html += f"""
+						<div style="
+							text-align: center;
+							font-size: {template.font_size * 0.8}px;
+							color: #555;
+							margin-bottom: 1mm;
+						">{data['item_code']}</div>
+					"""
+				
+				# Batch No
+				if template.include_batch_no and data.get("batch_no"):
+					label_html += f"""
+						<div style="
+							text-align: center;
+							font-size: {template.font_size * 0.8}px;
+							color: #555;
+						">Batch: {data['batch_no']}</div>
+					"""
+				
+				# Serial No
+				if template.include_serial_no and data.get("serial_no"):
+					label_html += f"""
+						<div style="
+							text-align: center;
+							font-size: {template.font_size * 0.8}px;
+							color: #555;
+						">S/N: {data['serial_no']}</div>
+					"""
+				
+				# Expiry Date
+				if template.include_expiry_date and data.get("expiry_date"):
+					label_html += f"""
+						<div style="
+							text-align: center;
+							font-size: {template.font_size * 0.8}px;
+							color: #555;
+						">Exp: {data['expiry_date']}</div>
+					"""
+				
+				# Price
+				if template.include_price and data.get("price"):
 					price_val = frappe.format_value(data['price'], {'fieldtype': 'Currency'})
 					label_html += f"""
 						<div style="
-							border-right: 1.2pt solid #000000;
-							border-bottom: 1.2pt solid #000000;
-							width: fit-content;
-							padding: 1px 4px;
+							text-align: center;
 							font-weight: bold;
-							font-size: {template.font_size * 1.1}px;
+							font-size: {template.font_size * 1.2}px;
+							margin: 1mm 0;
 						">{price_val}</div>
 					"""
 				
-				label_html += """
-					<div style="
-						flex-grow: 1;
-						display: flex;
-						flex-direction: column;
-						align-items: center;
-						justify-content: center;
-						padding: 2px;
-					">
-				"""
-
+				# Barcode (always at bottom)
 				if data.get("barcode_image"):
-					label_html += f"<img src='{data['barcode_image']}' style='max-width: 95%; max-height: 60%;' />"
+					label_html += f"""
+						<div style="
+							text-align: center;
+							flex-grow: 1;
+							display: flex;
+							align-items: center;
+							justify-content: center;
+						">
+							<img src='{data['barcode_image']}' style='max-width: 100%; max-height: 100%;' />
+						</div>
+					"""
 				
-				if template.include_item_code and data.get("item_code"):
-					label_html += f"<div style='font-size: {template.font_size * 0.8}px; margin-top: 1px;'>{data['item_code']}</div>"
-					
-				label_html += "</div></div></div>"
+				label_html += "</div>"
 			
 			html += label_html
 			
