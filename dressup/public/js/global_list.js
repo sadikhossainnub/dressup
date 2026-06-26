07063abd5e0d35a9f4985173c73b009cc8e6a3ef
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-06-26 20:53:00 +0600

    refactor: improve owner field detection and retrieval in global list view settings

--- dressup/public/js/global_list.js
+++ dressup/public/js/global_list.js
@@ -2,16 +2,19 @@
 frappe.listview_settings[""] = frappe.listview_settings[""] || {};
 
 $(document).on("app_ready", function () {
-    const _get_fields = frappe.views.ListView.prototype.get_fields;
-    frappe.views.ListView.prototype.get_fields = function () {
-        let fields = _get_fields.call(this);
-        // owner already a default_field, just push if not present
-        const has_owner = fields.find(
-            (f) => (Array.isArray(f) ? f[0] : f) === "owner"
-        );
-        if (!has_owner) {
-            fields.push(["owner", this.doctype]);
-        }
-        return fields;
-    };
+	const _get_fields = frappe.views.ListView.prototype.get_fields;
+	frappe.views.ListView.prototype.get_fields = function () {
+		let fields = _get_fields.call(this);
+		// owner already a default_field, just push if not present
+		const has_owner = fields.some((f) => {
+			if (typeof f === "string") {
+				return f === "owner" || f.endsWith(".`owner`") || f.endsWith(".owner");
+			}
+			return (Array.isArray(f) ? f[0] : f) === "owner";
+		});
+		if (!has_owner) {
+			fields.push(frappe.model.get_full_column_name("owner", this.doctype));
+		}
+		return fields;
+	};
 });
