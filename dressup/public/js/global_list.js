9563ad28aadfb63e85fde3fada57fe7f2bf25018
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-06-26 21:50:12 +0600

    refactor: remove redundant owner field injection from list view settings

--- dressup/public/js/global_list.js
+++ dressup/public/js/global_list.js
@@ -1,20 +1,4 @@
-// Add "Created By" (owner) column to all DocType list views
-frappe.listview_settings[""] = frappe.listview_settings[""] || {};
-
-$(document).on("app_ready", function () {
-	const _get_fields = frappe.views.ListView.prototype.get_fields;
-	frappe.views.ListView.prototype.get_fields = function () {
-		let fields = _get_fields.call(this);
-		// owner already a default_field, just push if not present
-		const has_owner = fields.some((f) => {
-			if (typeof f === "string") {
-				return f === "owner" || f.endsWith(".`owner`") || f.endsWith(".owner");
-			}
-			return (Array.isArray(f) ? f[0] : f) === "owner";
-		});
-		if (!has_owner) {
-			fields.push(frappe.model.get_full_column_name("owner", this.doctype));
-		}
-		return fields;
-	};
-});
+// "Created By" (owner) field for all DocType list views.
+// NOTE: `owner` is already included in frappe.model.std_fields_list,
+// so it is fetched automatically by every list view. No override needed.
+// The visible column injection is handled by global_owner_column.js.
