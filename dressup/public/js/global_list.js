c7ceb036efeabd74bc3530476c73435ac6dfb189
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-06-26 21:55:12 +0600

    refactor: remove global owner column injection scripts and associated hook registration

--- dressup/public/js/global_list.js
+++
@@ -1,4 +0,0 @@
-// "Created By" (owner) field for all DocType list views.
-// NOTE: `owner` is already included in frappe.model.std_fields_list,
-// so it is fetched automatically by every list view. No override needed.
-// The visible column injection is handled by global_owner_column.js.
