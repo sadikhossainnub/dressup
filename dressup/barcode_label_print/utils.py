7780f82c7636e5d8db1f54b94983e47a20f9991c
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-08-04 15:11:24 +0600

    feat: make barcode dimensions configurable and adjust layout and QR border for RMG2 print format

--- dressup/barcode_label_print/utils.py
+++ dressup/barcode_label_print/utils.py
@@ -84,7 +84,7 @@ def get_qr_base64(data):
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=10,
-            border=4,
+            border=1,
         )
         qr.add_data(data)
         qr.make(fit=True)
